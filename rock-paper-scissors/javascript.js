9a3da73168fb2b92bd49f6db63d977ea5c83634b
dinkdonk0 <128898053+dinkdonk0@users.noreply.github.com>
2023-03-28 23:34:42 +0200

    added game()  - added capital letters on functions

--- rock-paper-scissors/javascript.js
+++ rock-paper-scissors/javascript.js
@@ -1,7 +1,7 @@
 console.log("Rock paper scissors");
 
 
-function getComputerChoice(){
+function GetComputerChoice(){
 
     const rndInt = Math.floor(Math.random() * 3) + 1
     let computerChoice = "";
@@ -18,15 +18,16 @@ function getComputerChoice(){
     return computerChoice;
 }
 
-function playerSelection(){
-    const playerChoice = prompt("Choose rock, paper or scissors");
+function PlayerSelection(){
+    let playerChoice = prompt("Choose rock, paper or scissors. Type 0 to end game.");
     playerChoice.toLowerCase();
     return playerChoice;
 }
 
 
-
-function playRound(playerChoice, computerSelection) {
+let playerPoints = 0;
+let computerPoints = 0;
+function PlayRound(playerChoice, computerSelection) {
     console.log("Player chose " + playerChoice + " and the computer chose " + computerSelection)
     if(playerChoice===computerSelection){
         return "Even!";
@@ -34,13 +35,38 @@ function playRound(playerChoice, computerSelection) {
     else if((playerChoice==="rock" && computerSelection==="scissors")||
             (playerChoice==="paper" && computerSelection==="rock")||
             (playerChoice==="scissors" && computerSelection==="paper")){
+                playerPoints++;
         return "Player won!"
     }
     else{
+        computerPoints++;
         return "Computer won!"
     }
   }
 
-  const playerChoice = playerSelection();
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerChoice, computerSelection));
+
+
+  function Game(){
+  
+    while (true){
+        if(playerPoints>4||computerPoints>4){
+            break;
+        }
+        let playerChoice = PlayerSelection();
+        if(playerChoice!=="rock" && playerChoice!=="paper" && playerChoice!=="scissors"){
+            break;
+            
+        }
+        else{
+        let computerSelection = GetComputerChoice();
+        console.log(PlayRound(playerChoice, computerSelection));
+        console.log("The score is currently: " + playerPoints + " to " + computerPoints);
+        }
+        
+        
+    }
+    return "Game ended with a score of " + playerPoints + " to " + computerPoints;
+    }
+  
+ 
+console.log(Game());
