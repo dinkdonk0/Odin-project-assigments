a271e04e5f99c8b5ebb7024633bd577b1cb62fb2
dinkdonk0 <128898053+dinkdonk0@users.noreply.github.com>
2023-04-03 19:39:10 +0200

    changed generate tile, added console log for test

--- etch-a-sketch/javascript.js
+++ etch-a-sketch/javascript.js
@@ -1,13 +1,20 @@
 const mainContainer = document.querySelector(".mainContainer");
+let tileBlocks = []; //array that all tileblocks will go in to
 
 function generateTile(){
-//let tileBlocks = document.createElement("div");
-    for(let i=0;i<16;i++){
-        let tileBlocks = document.createElement("div");
-        mainContainer.appendChild(tileBlocks);
-        //tileBlocks.style.border = "thick solid #0000FF";
+
+    for(let i=0;i<16;i++){ //creates tiles and puts them in the array :)
         
-      
+        let tileBlock = document.createElement("div");
+        mainContainer.appendChild(tileBlock); 
+        tileBlocks.push(tileBlock); 
     }
 }
+
 generateTile();
+
+for(let i = 0; i < tileBlocks.length; i++){
+    tileBlocks[i].addEventListener("mouseover", function(){
+        console.log("so far so good");
+    });
+}
