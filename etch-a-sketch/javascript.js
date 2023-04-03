f826532f1fcf4ff0ba365cfc4fd3e8c921b4c698
dinkdonk0 <128898053+dinkdonk0@users.noreply.github.com>
2023-04-03 20:25:24 +0200

    added stylegrid

--- etch-a-sketch/javascript.js
+++ etch-a-sketch/javascript.js
@@ -1,20 +1,37 @@
 const mainContainer = document.querySelector(".mainContainer");
+const resetButton = document.querySelector(".reset");
 let tileBlocks = []; //array that all tileblocks will go in to
 
 function generateTile(){
 
-    for(let i=0;i<16;i++){ //creates tiles and puts them in the array :)
-        
+    for(let i=0;i<256;i++){ //creates tiles and puts them in the array :)
+       
         let tileBlock = document.createElement("div");
         mainContainer.appendChild(tileBlock); 
         tileBlocks.push(tileBlock); 
     }
+  
+    
+}
+function styleGrid(){
+    mainContainer.style.gridTemplateColumns = "repeat(16, 1fr)";
+    mainContainer.style.gridTemplateRows = "repeat(16, 1fr)";
 }
 
+
+//run functions
 generateTile();
+styleGrid();
+
 
 for(let i = 0; i < tileBlocks.length; i++){
     tileBlocks[i].addEventListener("mouseover", function(){
-        console.log("so far so good");
+        this.style.backgroundColor = "blue";
     });
 }
+
+resetButton.addEventListener("click", function(){
+    for(let i = 0; i < tileBlocks.length; i++){
+        tileBlocks[i].style.backgroundColor = "white";
+    }
+})
