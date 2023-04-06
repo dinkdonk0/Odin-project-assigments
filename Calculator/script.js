d4991890f022a617afb6e22540ceadd3ee893f31
dinkdonk0 <128898053+dinkdonk0@users.noreply.github.com>
2023-04-06 21:38:48 +0200

    added math functions

--- Calculator/script.js
+++ Calculator/script.js
@@ -37,3 +37,28 @@ mathButtons.forEach(button => {
       numDisplay.textContent+=button.textContent;
     });
   });
+
+
+
+
+  function add(num1, num2){
+    return num1+num2;
+  }
+
+  function subtract(num1, num2){
+    return num1-num2;
+  }
+
+  function multiply(num1, num2){
+    return num1*num2;
+  }
+
+  function divide(num1, num2){
+    if(num2==0){
+        return null;
+    }
+    else{
+    return num1/num2;
+    }
+    
+  }
