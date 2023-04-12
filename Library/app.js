6bd9b2f2a6145ea54244c8ba52e56b09c391efa3
dinkdonk0 <128898053+dinkdonk0@users.noreply.github.com>
2023-04-12 09:38:08 +0200

    toggleBtn bugfix

--- Library/app.js
+++ Library/app.js
@@ -3,13 +3,18 @@ const toggleBtn = document.getElementById('toggleBtn');
 const bookAdder = document.querySelector('.bookAdder');
 
 // Add a click event listener to the button
-toggleBtn.addEventListener('click', function() {
+toggleBtn.addEventListener('click', function () {
+    // Get the computed style of the div
+    const computedStyle = window.getComputedStyle(bookAdder);
+
     // Check the current display value of the div
-    if (bookAdder.style.display === 'none') {
+    if (computedStyle.display === 'none') {
         // If the div is hidden, display it
         bookAdder.style.display = 'block';
+        toggleBtn.textContent = "Hide"
     } else {
         // If the div is visible, hide it
         bookAdder.style.display = 'none';
+        toggleBtn.textContent = "Add book"
     }
 });
