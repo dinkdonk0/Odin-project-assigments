03a903b61570e8a396660a0e46e2c51462e6d112
dinkdonk0 <128898053+dinkdonk0@users.noreply.github.com>
2023-04-20 09:49:02 +0200

    added basic functions

---
+++ Todo-list/main.js
@@ -0,0 +1,9 @@
+const addTask = document.querySelector(".addTask");
+const  taskBox = document.querySelector(".addTask2");
+
+
+addTask.addEventListener("click", ()=>{
+    taskBox.classList.toggle("hidden");
+})
+
+
