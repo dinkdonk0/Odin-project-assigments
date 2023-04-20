1ee9b7735127b573147dcd70a9b032e001d2bf67
dinkdonk0 <128898053+dinkdonk0@users.noreply.github.com>
2023-04-20 10:34:49 +0200

    added button eventlisteners

--- Todo-list/main.js
+++ Todo-list/main.js
@@ -1,9 +1,20 @@
 const addTask = document.querySelector(".addTask");
-const  taskBox = document.querySelector(".addTask2");
+const taskBox = document.querySelector(".addTask2");
+const mainContent = document.querySelector(".maincontent");
+const addButton = document.querySelector("#button1");
+const cancelButton = document.querySelector("#button2");
+
+taskBox.classList.add("hidden");
 
 
-addTask.addEventListener("click", ()=>{
+function swapDivs(){
     taskBox.classList.toggle("hidden");
-})
+    addTask.classList.toggle("hidden");
+}
 
+addTask.addEventListener("click", swapDivs);
+
+cancelButton.addEventListener("click", swapDivs);
+
+addButton.addEventListener("click", swapDivs);
 
