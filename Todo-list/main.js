2e94b73ba2bce9db23446f02061324681562492d
dinkdonk0 <128898053+dinkdonk0@users.noreply.github.com>
2023-04-21 20:13:29 +0200

    added task add functionality

--- Todo-list/main.js
+++ Todo-list/main.js
@@ -3,6 +3,8 @@ const taskBox = document.querySelector(".addTask2");
 const mainContent = document.querySelector(".maincontent");
 const addButton = document.querySelector("#button1");
 const cancelButton = document.querySelector("#button2");
+const textInput = document.getElementById('myTextInput');
+const tasks = document.querySelector(".tasks");
 
 taskBox.classList.add("hidden");
 
@@ -12,9 +14,23 @@ function swapDivs(){
     addTask.classList.toggle("hidden");
 }
 
+function taskAdder(){
+    const inputValue = textInput.value;    
+        if (inputValue.trim() === '') {
+            alert('Please enter a task');
+            return;
+        }
+    const newDiv = document.createElement('div');
+    newDiv.textContent = inputValue;
+    tasks.appendChild(newDiv);
+    textInput.value = "";
+    swapDivs();
+}
+
+
 addTask.addEventListener("click", swapDivs);
 
 cancelButton.addEventListener("click", swapDivs);
 
-addButton.addEventListener("click", swapDivs);
+addButton.addEventListener("click", taskAdder);
 
